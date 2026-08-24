55efff8b12a5c21e73cee1bbaebc3cc7b6af57d0
ayat7865-creator <ayatp7865@gmail.com>
2026-08-24 11:18:54 +0530

    project

---
+++ day7q14.c
@@ -0,0 +1,15 @@
+#include<stdio.h.>
+int main()
+{
+double character;
+int a, e, i, o, u;
+if (character = a, e, i, o, u)
+{
+printf ("The character is VOWEL.\n");
+}
+else
+{
+printf ("The character is CONSONANT.\n");
+}
+return 0;
+}
