45bb0fa7e1cf541d320399c46806896b571ad106
ayat7865-creator <ayatp7865@gmail.com>
2026-08-30 22:09:42 +0530

    main

--- day20q39.c
+++ day20q39.c
@@ -1,3 +1,18 @@
+Q39: Write a program to find the product of odd digits of a number.
+
+/*
+Sample Test Cases:
+Input 1:
+12345
+Output 1:
+15 (1*3*5)
+
+Input 2:
+2468
+Output 2:
+1 (no odd digits, assume 1)
+
+*/
 #include <stdio.h>
 #include <stdlib.h> 
 int main() {
--- day20q40.c
+++ day20q40.c
@@ -1,3 +1,18 @@
+Q40: Write a program to find the 1’s complement of a binary number and print it.
+
+/*
+Sample Test Cases:
+Input 1:
+1010
+Output 1:
+0101
+
+Input 2:
+1111
+Output 2:
+0000
+
+*/
 #include <stdio.h>
 #include <string.h>
 #define MAX_SIZE 100
